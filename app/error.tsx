74608e6204c6fc850a347e4a7f27dd0070fb0a7a
mordecai <mordecai.g.men@gmail.com>
2024-12-20 20:41:17 -0500

    linting.

--- app/error.tsx
+++ app/error.tsx
@@ -1,19 +1,13 @@
 "use client"; // Error boundaries must be Client Components
 
-export default function GlobalError({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
+export default function GlobalError() {
   return (
     // global-error must include html and body tags
     <html>
       <body>
         <div className="absolute left-1/2 top-1/2 flex items-center justify-center bg-black">
           <h2 className="text-3xl font-thin">404</h2>
-          <p>That page doesn't exist</p>
+          <p>That page doesn&apost exist</p>
         </div>
       </body>
     </html>
